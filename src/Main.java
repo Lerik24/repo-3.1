f26f8926d33adc8ea9a36fdf7f65ae92dad47159
Lerik <leramartea24@gmail.com>
2024-08-06 16:23:42 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 30025;
+        int koef = 20;
+        int mile = price / koef ;
+
+        System.out.println(mile);
+
+    }
+}
